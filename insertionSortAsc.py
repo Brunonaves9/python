a8f13f1f4432d99c0bebad210b40b9226fe7395a
Brunonaves9 <brunomnaves07@gmail.com>
2022-11-15 13:13:29 -0300

    Insertion Sort em ordem crescente

---
+++ insertionSortAsc.py
@@ -0,0 +1,14 @@
+def insertionSortAsc(A):
+    
+    for i in range(1, len(A)):
+        key = A[i]
+        j = i -1
+        while j >= 0 and A[j] > key:
+            j = j - 1
+        A[j + 1] = key
+
+    return A 
+
+A = [31, 41, 59, 62, 42, 58]
+
+print(insertionSortAsc(A))
