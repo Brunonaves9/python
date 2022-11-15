bf87ce681dc495f2f413b5a5c7b5b0da1253166a
Brunonaves9 <brunomnaves07@gmail.com>
2022-11-15 13:20:42 -0300

    Selection Sort

---
+++ selectionSort.py
@@ -0,0 +1,15 @@
+def ordenacao_selecao(A):
+    n = len(A)
+    # Percorre o arranjo A.
+    for i in range(n):
+        # Encontra o elemento mínimo em A.
+        minimo = i
+        for j in range(i + 1, n):
+            if A[minimo] > A[j]:
+                minimo = j
+        # Coloca o elemento mínimo na posição correta.
+        A[i], A[minimo] = A[minimo], A[i]
+    return A
+
+A = [10,5,6,3,9,1,8,2,4,7]
+print(ordenacao_selecao(A))
