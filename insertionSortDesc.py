cb4e9498025e15bb618f56f4db29edb0bbe455ea
Brunonaves9 <brunomnaves07@gmail.com>
2022-11-15 13:18:46 -0300

    Ajuste

--- insertionSortDesc.py
+++ insertionSortDesc.py
@@ -3,7 +3,7 @@ def insertionSortAsc(A):
     for i in range(1, len(A)):
         key = A[i]
         j = i -1
-        while j >= 0 and A[j] > key:
+        while j >= 0 and key > A[j]:
             A[j + 1] = A[j]
             j = j - 1
         A[j + 1] = key
